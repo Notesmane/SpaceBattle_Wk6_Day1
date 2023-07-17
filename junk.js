2eab553b8a5c16a2afb03482ceeee7e7414b9a9b
Ronnie Powell <notesmane@mac-pro.myfiosgateway.com>
2023-07-16 23:35:46 -0400

    Space Battle Completed (w/minor bugs)

---
+++ junk.js
@@ -0,0 +1,81 @@
+// const alienShip2 = new BattleShip(alienHull, alienFire, alienAccuracy);
+// const alienShip3 = new BattleShip(alienHull, alienFire, alienAccuracy);
+// const alienShip4 = new BattleShip(alienHull, alienFire, alienAccuracy);
+// const alienShip5 = new BattleShip(alienHull, alienFire, alienAccuracy);
+// const alienShip6 = new BattleShip(alienHull, alienFire, alienAccuracy);
+// console.log(alienShip2);
+// console.log(alienShip3);
+// console.log(alienShip4);
+// console.log(alienShip5);
+// console.log(alienShip6);
+
+
+//^ ===========================================================
+
+// const alienHull = (
+//     function getRandomArbitrary(3, 5) {
+//         return Math.random() * (5 - 3) + 3;
+//       }
+
+// );
+
+
+//^ ===========================================================
+
+
+//& below is button functionality grabbed from event-practice script page, it didnt have a style page
+// const propBtn = document.getElementById('prop-btn');
+// const methodBtn = document.querySelector('#method-btn');
+
+// // registers a function to the onClick event
+// propBtn.onclick = print;
+
+// function print() {
+//     console.log('Button was clicked!');
+// }
+
+// // the method approach that will register a function to the selected element
+// // ('click' = event listener, print = function we want to call)
+// methodBtn.addEventListener('click', print);
+
+// methodBtn.addEventListener('mousedown', function() {
+//     console.log('Mouse Down Event');
+// });
+
+// methodBtn.addEventListener('mousemove', function() {
+//     console.log('Mouse is moving...');
+// });
+
+// methodBtn.addEventListener('mouseup', function() {
+//     console.log('Mouse Up Event');
+// });
+
+//^ ===========================================================
+
+// //* Alien random properties
+// const alienHull = Math.floor(Math.random()*3)+3;
+// console.log(alienHull);
+// const alienPower = Math.random()*2 + 2;
+// console.log(alienFire.toFixed(2));
+// const alienAccuracy = Math.random()*0.2 + 0.6;
+// console.log(alienAccuracy.toFixed(2));
+// const alienHit = humanShip(1) - alienHull;
+// console.log(alienHit);
+
+
+//^ ===========================================================
+
+// {
+    //     getHullDown() {
+        //         return this.
+        //     }
+        // };
+        
+        // console.log(humanShip);
+        // const alienShip = new SpaceShip(alienHull, alienPower, alienAccuracy);
+        // console.log(alienShip);
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //! Problem Source: https://perscholas.instructure.com/courses/1513/pages/mini-project-slash-sba-save-the-universe?module_item_id=965412
 
 //^ A game round would look like this:
@@ -12,7 +11,7 @@
 // You lose the game if you are destroyed
 
 //^ Ship Properties
-// hull is the same as hitpoints. If hull reaches 0or less, the ship is destroyed
+// hull is the same as hit points. If hull reaches 0or less, the ship is destroyed
 // firepower is the amount of damage done to the hull of the target with a successful hit
 // accuracy is the chance between 0 and 1 that the ship will hit its target
 // Your spaceship, the USS Assembly should have the following properties:
@@ -34,21 +33,42 @@ class SpaceShip {
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
+    getAccuracy () {
+        return this.accuracy
+    }
+    getHull () {
+        return this.hull
+    }
+    getFirePower () {
+        return this.firepower
+    }
+    hullDown (num) {
+        this.hull -= num
+    }
 };
 
-//* Alien random properties
-const alienHull = Math.floor(Math.random()*3)+3;
-// console.log(alienHull);
-const alienPower = Math.random()*2 + 2;
-// console.log(alienFire.toFixed(2));
-const alienAccuracy = Math.random()*0.2 + 0.6;
-// console.log(alienAccuracy.toFixed(2));
+let randomNumber = Math.random();
+
+const humanShip = new SpaceShip(20, 5, .7); 
+const alienShip = new SpaceShip( Math.floor(Math.random()*4)+8, Math.random()*2 + 2, Math.random()*0.2 + 0.6)
 
 
-const humanShip = new SpaceShip(20, 5, .7);
-console.log(humanShip);
-const alienShip = new SpaceShip(alienHull, alienPower, alienAccuracy);
-console.log(alienShip);
+humanShip.hullDown(alienShip.firepower);
+alienShip.hullDown(humanShip.firepower);
+
+
+// let alienHorde = [alienShip, alienShip, alienShip, alienShip, alienShip, alienShip];
+
+//^ ======================================= gets random number of alien ships
+// gets a random number between 1 and 6 for the possible amount of alien ships
+function getRandomNumber() { 
+    const randomDecimal = Math.random();
+    const scaledNumber = randomDecimal * 5 + 1;
+    const roundedNumber = Math.round(scaledNumber);
+    return roundedNumber;
+  }
+  const numAlienShips = getRandomNumber();
+  console.log(`The number of Alien ships you will face is: ${numAlienShips}`);
 
 
 //* Game intro
@@ -56,39 +76,189 @@ console.log(alienShip);
 // 2. then it should prompt the player to enter his/her name
 // 3. then it should prompt user to attack the alien ship, then goto Attack mechanism STEP1
 
+alert("Welcome to 'Space Battle'!");
+// alert("Where you will have to fight the alien horde to save Earth from the invading Aliens."); //// domination instead? invaders?
+// alert("Your ship is well equipped and has a great crew! It also has a very strong forcefield and hull.");
+// alert("You have some of the best marksmen in the galaxy so your firing accuracy is remarkable,");
+// alert("And your torpedos are super strong compared to other species.");
+// alert("The aliens ships are weaker, arent the best with their aim and have less effective torpedos.");
+// alert("(Not sure how this is possible considering they traveled from accross the galaxy but...)");
+alert("Attack the first ship if you dare, and start the SPACE BATTLE!");
+promptFirstAttack();
+
+
+
+//// let start = prompt('Do you want to attack the alien ship? (Type \'yes\' or \'no\')');
+//// if (start === "yes") {
+////     document.getElementById("attack");
+//// };
+
+function promptFirstAttack() {
+    let answer = prompt("Do you want to attack the first alien ship? (Type yes or no)");
+    answer = answer.toLowerCase();
+    if (answer === "yes") {
+        // User wants to attack the alien ship
+        console.log("\u001b[36m========== You're about to wage war... prepare for battle! ==========\u001b[0m");
+        humansAttack();
+        // Add your logic for the attack here
+      } else if (answer === "no") {
+        // User does not want to attack the alien ship
+        console.log("You chose not to attack. COWARD! ");
+        alert("The alien race has destroyed your ship and taken over the Earth.");
+        gameOver();
+        // Add your logic for not attacking here
+      } else {
+        // Invalid response
+        console.log("\u001b[31mInvalid response. Please answer with 'yes' or 'no'.\u001b[0m");
+        promptFirstAttack();
+     }
+}
+
+function promptNextAttack() {
+    let answer = prompt("Do you want to attack the next alien ship? (Type yes or no)");
+    answer = answer.toLowerCase();
+    if (answer === "yes") {
+        // User wants to attack the alien ship
+        console.log("========== Prepare for battle! ==========");
+        humansAttack();
+        // Add your logic for the attack here
+      } else if (answer === "no") {
+        // User does not want to attack the alien ship
+        console.log("Seriously?? You retreated? COWARD! ");
+        alert("The alien Race has destroyed your ship and taken over the Earth.")
+        gameOver();
+        // Add your logic for not attacking here
+      } else {
+        // Invalid response
+        console.log("\u001b[31mInvalid response. Please answer with 'yes' or 'no'.\u001b[0m");
+        promptNextAttack();
+     }
+}
+
+//// let start = prompt (readyToAttack(qestion));
+
+//^ ======================================================================== This is the end of the Game Intro (I think lol)
+
+
+// console.log(alienHorde[2]);
+
 //* Attack mechanism: function, method or loop?
-// 1. humanAttacks alienShip log humanFired
-// 2. boolean: did humaShip hit alienShip, yes or no?
-// 3. if yes, subtract humanFire from alienHull, log alienShipHit, goto STEP5
-// 4. if no, log alienShipMissed goto STEP5
-// 5. if alienHull is 0 or less then log alienShipDestroyed break loop and goto NextBattle STEP1
-// 6. if alienHull is greater than 0 goto STEP7
-// 7. alienAttacks humaShip
-// 8. boolean: did alienShip hit humanShip, if no goto STEP1
-// 7. if yes, subtract alienFire from humanHull
-// 8. if humanHull is 0 or less break loop and log humanShipDestroyed 
-// 9. if humanHull is greater than 0 goto STEP1
-// 10. 
+// // when humanShip fires there should be a string logged "You fired on the alien ship."
 
-//* Next Battle mechanism: function, method or loop?
-// 1. access next alien ship
-// 2. boolean: is next alienShip between alienship 1-6?
-// 3. if yes, prompt user nextShip if yes then goto STEP6
-// 4. if no, then goto STEP7
-// 5. log humanCoward then break loop and goto GameOver STEP1
-// 6. break loop then goto Attack mechanism STEP1 
-// 7. if user says yes, then goto gameIntro
-// 8. log aliensDefeated, break loop then goto GameOver STEP1
-//// 5. if yes and user wants to fight another alien ship then goto STEP7
+function fightCounter() {
+    for (let counter = 1; counter <= numAlienShips; counter++) {
+        if (counter === numAlienShips) {
+            winner();
+        }
+        else {
+        humansAttack();
+        }
+    }
+}
 
+    //^ ======================================= humans attack aliens
+    function humansAttack() {
+        setTimeout(() => { console.log("You fired on the alien ship.");}, 3000);
+        if (randomNumber <= humanShip.accuracy) {
+            alienShip.hullDown(humanShip.firepower)
+            // console.log(alienShip.hull);
+            setTimeout(() => { console.log("\u001b[32mGreat Job! You hit the alien ship!\u001b[0m");}, 6000); {
+                if (alienShip.hull <= 0) {
+                    setTimeout(() => { console.log("\u001b[32mKaboooooom... you destroyed the alien ship!\u001b[0m");}, 9000);
+                    setTimeout(() => { promptNextAttack();}, 12000);
+                    randomNumber = Math.random();
+                }
+                else { 
+                    setTimeout(() => { console.log(`The aliens hull capacity is now at: ${alienShip.hull}`);}, 9000);
+                    setTimeout(() => { aliensAttack();}, 9000);
+                    randomNumber = Math.random();
+                } 
+                
+                // console.log("Alien Hull Power is now:" humanShip.firepower - alienShip.hull);
+            } 
+        } else {
+            setTimeout(() => { console.log("\u001b[33mSo close, but you missed the alien ship!\u001b[0m");}, 9000);
+            setTimeout(() => { aliensAttack();}, 15000);
+            randomNumber = Math.random();
+        } 
+        
+    }
+    
+    //^ ======================================= aliens attack humans
+    function aliensAttack() {
+        setTimeout(() => { console.log("The alien ship fired on you.");}, 3000); 
+        if (randomNumber <= alienShip.accuracy) {
+            humanShip.hullDown(alienShip.firepower)
+            setTimeout(() => { console.log("\u001b[33mOuchie... the alien ship hit you.\u001b[0m"); }, 6000); {
+                if (humanShip.hull <= 0) {
+                    console.log("\u001b[31mYou have been Destroyed!\u001b[0m");
+                    gameOver();
+                    randomNumber = Math.random();
+                }
+            }
+            setTimeout(() => { console.log(`Your hull capacity is now at: ${humanShip.hull}`); }, 9000); 
+            setTimeout(() => { humansAttack(); }, 9000); 
+            randomNumber = Math.random();
+        } else {
+            setTimeout(() => { console.log("\u001b[32mGreat manuever, the alien ship missed you\u001b[0m!"); }, 6000); 
+            setTimeout(() => { humansAttack(); }, 6000); 
+            randomNumber = Math.random();
+        }
+    }
+    
+    // 1. humanAttacks alienShip log humanFired
+    // 2. boolean: did humaShip hit alienShip, yes or no?
+    // 3. if yes, subtract humanFire from alienHull, log alienShipHit, goto STEP5
+    // 4. if no, log alienShipMissed goto STEP5
+    // 5. if alienHull is 0 or less then log alienShipDestroyed break loop and goto NextBattle STEP1
+    // 6. if alienHull is greater than 0 goto STEP7
+    // 7. alienAttacks humaShip
+    // 8. boolean: did alienShip hit humanShip, if no goto STEP1
+    // 7. if yes, subtract alienFire from humanHull
+    // 8. if humanHull is 0 or less break loop and log humanShipDestroyed 
+    // 9. if humanHull is greater than 0 goto STEP1
+    
+    //^ ======================================================================== This is the end of the Attack Mechanism
 
+    
 //* Game Over Mechanism
+// if the human ship is destroyed, then a "game over" string needs to be logged
 // 1. log gameOver
+    
+function gameOver() {
+    console.log("Game Over!"); 
+    setTimeout(() => { playAgain();}, 4000);
+}
+
+function winner() {
+    console.log("HORRRAAAYY!!!! You have destroyed all of the invading ships, and saved Earth!")
+    playAgain();
+}
+
+function playAgain() {
+    let answer = prompt("Do you want to play agian? (Type yes or no)");
+    answer = answer.toLowerCase();
+    if (answer === "yes") {
+        // User wants to attack the alien ship
+        console.log("Prepare for battle!");
+        humansAttack();
+        // Add your logic for the attack here
+    } else if (answer === "no") {
+        // User does not want to attack the alien ship
+        console.log("\u001b[32mOk.. Have a nice day!\u001b[0m");
+        // Add your logic for not attacking here
+    } else {
+        // Invalid response
+        console.log("\u001b[31Invalid response. Please answer with 'yes' or 'no'.\u001b[0");
+    }
+}
+    
 // 2. prompt user playAgain
 // 3. if yes then goto gameIntro
 // 4. if no then do nothing???
 
 
+//^ ======================================================================== This is the end of the Game Over Mechanism
 
 //* Human Ship attack
 // human ship attacks alien ship
@@ -101,112 +271,54 @@ console.log(alienShip);
 // then the loop starts over, until one of the ships is destroyed
 // if the human player decides to fight another alien then the loop starts back over (iterating to a max of 6 total fights)
 
-
-//* Console logged strings
-// when alienShip fires there should be a string logged "INCOMING!! The alien ship fired at you!"
-const alienFired = "INCOMING!! The alien ship fired at you!";
-console.log(alienFired);
-// if the alien ship was hit then there needs to be an "alien ship hit" string logged
-const alienShipHit = "Great Job! You hit the alien ship!";
-console.log(alienShipHit);
-// if the alien ship was not hit then there needs to be an "alien ship missed" string logged
-const alienShipMissed = "So close, but you missed the alien ship!";
-console.log(alienShipMissed);
-// then if the alien ship was destroyed, an "alien ship destroyed" string needs to be logged
-const alienShipDestroyed = "Kaboooooom... you destroyed the alien ship!";
-console.log(alienShipDestroyed);
-// when humanShip fires there should be a string logged "You fired on the alien ship."
-const humanFired = "You fired on the alien ship.";
-console.log(humanFired);
-// if the human ship is hit, then a "you were hit" string needs to be logged
-const humanShipHit = "Ouchie... the alien ship hit you.";
-console.log(humanShipHit);
-// if the human ship is not hit, then a "the alien ship missed" string needs to be logged
-const humanShipMissed = "Great manuever, the alien ship missed you!";
-console.log(humanShipMissed);
-const humanCoward = "ON-SCREEN MESSAGE RECIEVED AND TRANSLATED: We always heard you Humans were COWARDS - no honor!"
-console.log(humanCoward);
-// if the alien ship is destroyed then it goes to the "Next Alien?" string
-const nextShip = "Do you want to battle the next alien ship?"
-console.log(nextShip);
-// if the human ship is destroyed a "your ship has been destroyed" string should log
-const humanShipDestroyed = "You have been Destroyed!";
-console.log(humanShipDestroyed);
-// if all of the aliens are defeated, log the string "AMAZING! You have defeated the alien hoard!"
-const aliensDefeated = "AMAZING! You have defeated the alien hoard!"
-console.log(aliensDefeated);
-// if the human ship is destroyed, then a "game over" string needs to be logged
-const gameOver = "Game Over!"
-console.log(gameOver);
-// once the game is over then ask the user "Do you want to play Space Battle again?
-const playAgain = "Do you want to play 'Space Battle' again?"
-console.log(playAgain);
-
-
-
-
-
-
-
-//& below is button functionality grabbed from event-practice script page, it didnt have a style page
-// const propBtn = document.getElementById('prop-btn');
-// const methodBtn = document.querySelector('#method-btn');
-
-// // registers a function to the onClick event
-// propBtn.onclick = print;
-
-// function print() {
-//     console.log('Button was clicked!');
+//* Next Battle mechanism: function, method or loop?
+// for (let i = 0; i < alienShipNumber.length; i++) {
+//     if ()
 // }
 
-// // the method approach that will register a function to the selected element
-// // ('click' = event listener, print = function we want to call)
-// methodBtn.addEventListener('click', print);
 
-// methodBtn.addEventListener('mousedown', function() {
-//     console.log('Mouse Down Event');
-// });
-
-// methodBtn.addEventListener('mousemove', function() {
-//     console.log('Mouse is moving...');
-// });
-
-// methodBtn.addEventListener('mouseup', function() {
-//     console.log('Mouse Up Event');
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const alienShip2 = new BattleShip(alienHull, alienFire, alienAccuracy);
-// const alienShip3 = new BattleShip(alienHull, alienFire, alienAccuracy);
-// const alienShip4 = new BattleShip(alienHull, alienFire, alienAccuracy);
-// const alienShip5 = new BattleShip(alienHull, alienFire, alienAccuracy);
-// const alienShip6 = new BattleShip(alienHull, alienFire, alienAccuracy);
-// console.log(alienShip2);
-// console.log(alienShip3);
-// console.log(alienShip4);
-// console.log(alienShip5);
-// console.log(alienShip6);
-
-
-// const alienHull = (
-//     function getRandomArbitrary(3, 5) {
-//         return Math.random() * (5 - 3) + 3;
-//       }
-
-// );
-
-
+// 1. access next alien ship
+// 2. boolean: is next alienShip between alienship 1-6?
+// 3. if yes, prompt user nextShip then goto STEP6
+// 5. log humanCoward then break loop and goto GameOver STEP1
+// 6. break loop then goto Attack mechanism STEP1 
+// 7. if user says yes, then goto gameIntro
+// 8. log aliensDefeated, break loop then goto GameOver STEP1
+//// 5. if yes and user wants to fight another alien ship then goto STEP7
+//^ ======================================================================== This is the end of the Next Battle Mechanism
+    
+    
+    
+//* Console logged strings
+    // // when alienShip fires there should be a string logged "INCOMING!! The alien ship fired at you!"
+// const alienFired = "INCOMING!! The alien ship fired at you!";
+// console.log(alienFired);
+// // if the alien ship was hit then there needs to be an "alien ship hit" string logged
+// const alienShipHit = "Great Job! You hit the alien ship!";
+// console.log(alienShipHit);
+// // if the alien ship was not hit then there needs to be an "alien ship missed" string logged
+// const alienShipMissed = "So close, but you missed the alien ship!";
+// console.log(alienShipMissed);
+// // then if the alien ship was destroyed, an "alien ship destroyed" string needs to be logged
+// const alienShipDestroyed = "Kaboooooom... you destroyed the alien ship!";
+// console.log(alienShipDestroyed);
+// // if the human ship is hit, then a "you were hit" string needs to be logged
+// const humanShipHit = "Ouchie... the alien ship hit you.";
+// console.log(humanShipHit);
+// // if the human ship is not hit, then a "the alien ship missed" string needs to be logged
+// const humanShipMissed = "Great manuever, the alien ship missed you!";
+// console.log(humanShipMissed);
+// const humanCoward = "ON-SCREEN MESSAGE RECIEVED AND TRANSLATED: We always heard you Humans were COWARDS - no honor!"
+// console.log(humanCoward);
+// // if the alien ship is destroyed then it goes to the "Next Alien?" string
+// const nextShip = "Do you want to battle the next alien ship?"
+// console.log(nextShip);
+// // if the human ship is destroyed a "your ship has been destroyed" string should log
+// const humanShipDestroyed = "You have been Destroyed!";
+// console.log(humanShipDestroyed);
+// // if all of the aliens are defeated, log the string "AMAZING! You have defeated the alien hoard!"
+// const aliensDefeated = "AMAZING! You have defeated the alien hoard!"
+// console.log(aliensDefeated);
+// // once the game is over then ask the user "Do you want to play Space Battle again?
+// const playAgain = "Do you want to play 'Space Battle' again?"
+// console.log(playAgain);
